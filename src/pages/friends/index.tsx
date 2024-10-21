055f77e0e600c463f721a1fe1cb014c465088164
Lian <153711411@qq.com>
2024-10-21 23:08:52 +0800

    妈的最烦装逼的人！

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -9,7 +9,7 @@ import { motion } from 'framer-motion'
 import styles from './styles.module.css'
 
 const TITLE = '友链'
-const DESCRIPTION = '有很多良友，胜于有很多财富。'
+const DESCRIPTION = '妈的最烦装逼的人！'
 const ADD_FRIEND_URL = 'https://github.com/lianlianlianlianlianlian/darklotus/edit/main/data/friends.tsx'
 const SITE_INFO = `
 title: 'Lian'
