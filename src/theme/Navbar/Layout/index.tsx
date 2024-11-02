6ba1c2fd2b9dab6af6e8c004068fe764d182c3ed
Lian <153711411@qq.com>
2024-11-02 15:01:31 +0800

    fix：导航栏bug修复

--- src/theme/Navbar/Layout/index.tsx
+++ src/theme/Navbar/Layout/index.tsx
@@ -6,6 +6,7 @@ import NavbarMobileSidebar from '@theme/Navbar/MobileSidebar';
 import clsx from 'clsx';
 import { type ComponentProps, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 import styles from './styles.module.css';
 
@@ -14,78 +15,84 @@ function NavbarBackdrop(props: ComponentProps<'div'>) {
 }
 
 export default function NavbarLayout({ children }: Props): JSX.Element {
-    const {
-        navbar: { hideOnScroll, style },
-    } = useThemeConfig();
-    const mobileSidebar = useNavbarMobileSidebar();
-    const { navbarRef, isNavbarVisible } = useHideableNavbar(hideOnScroll);
-
-    const location = useLocation();
-    const isHomePage = location.pathname === '/' || location.pathname === '/en/';
-
-    // 用于记录滚动和滚轮状态
-    const lastScrollTop = useRef(window.pageYOffset);
-    const wheelDirection = useRef(0);
-
-    // 监听鼠标滚轮事件，获取滚动方向
-    const handleWheel = (e) => {
-        wheelDirection.current = e.deltaY > 0 ? 1 : -1;
-    };
-
-    // 监听页面滚动事件，根据鼠标滚轮方向和页面滚动控制导航栏显示或隐藏
-    const handleScroll = () => {
-        const scrollTop = window.pageYOffset;
-        const navbar = document.querySelector('.navbar');
-
-        if (hideOnScroll) {
-            if (wheelDirection.current === -1 && scrollTop > 0) {
-                // 仅当鼠标滚轮向上滚动时显示导航栏
-                navbar.classList.remove(styles.navbarHidden);
-            } else if (wheelDirection.current === 1 || scrollTop !== 0) {
-                // 向下滚动或者点击跳转时保持隐藏
-                navbar.classList.add(styles.navbarHidden);
-            } else if (scrollTop === 0) {
-                // 页面滚动到顶部时显示导航栏
-                navbar.classList.remove(styles.navbarHidden);
-            }
-        }
-
-        lastScrollTop.current = scrollTop;
-    };
-
-    useEffect(() => {
-        window.addEventListener('wheel', handleWheel);
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('wheel', handleWheel);
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     return (
-        <nav
-            ref={navbarRef}
-            aria-label={translate({
-                id: 'theme.NavBar.navAriaLabel',
-                message: 'Main',
-                description: 'The ARIA label for the main navigation',
-            })}
-            className={clsx(
-                'navbar',
-                'navbar--fixed-top',
-                isHomePage && 'bg-transparent',
-                hideOnScroll && [styles.navbarHideable, !isNavbarVisible && styles.navbarHidden],
-                {
-                    'navbar--dark': style === 'dark',
-                    'navbar--primary': style === 'primary',
-                    'navbar-sidebar--show': mobileSidebar.shown,
-                },
-            )}
-        >
-            {children}
-            <NavbarBackdrop onClick={mobileSidebar.toggle} />
-            <NavbarMobileSidebar />
-        </nav>
+        <BrowserOnly fallback={<div>Loading...</div>}>
+            {() => {
+                const {
+                    navbar: { hideOnScroll, style },
+                } = useThemeConfig();
+                const mobileSidebar = useNavbarMobileSidebar();
+                const { navbarRef, isNavbarVisible } = useHideableNavbar(hideOnScroll);
+
+                const location = useLocation();
+                const isHomePage = location.pathname === '/' || location.pathname === '/en/';
+
+                // 用于记录滚动和滚轮状态
+                const lastScrollTop = useRef(window.pageYOffset);
+                const wheelDirection = useRef(0);
+
+                // 监听鼠标滚轮事件，获取滚动方向
+                const handleWheel = (e) => {
+                    wheelDirection.current = e.deltaY > 0 ? 1 : -1;
+                };
+
+                // 监听页面滚动事件，根据鼠标滚轮方向和页面滚动控制导航栏显示或隐藏
+                const handleScroll = () => {
+                    const scrollTop = window.pageYOffset;
+                    const navbar = document.querySelector('.navbar');
+
+                    if (hideOnScroll) {
+                        if (wheelDirection.current === -1 && scrollTop > 0) {
+                            // 仅当鼠标滚轮向上滚动时显示导航栏
+                            navbar.classList.remove(styles.navbarHidden);
+                        } else if (wheelDirection.current === 1 || scrollTop !== 0) {
+                            // 向下滚动或者点击跳转时保持隐藏
+                            navbar.classList.add(styles.navbarHidden);
+                        } else if (scrollTop === 0) {
+                            // 页面滚动到顶部时显示导航栏
+                            navbar.classList.remove(styles.navbarHidden);
+                        }
+                    }
+
+                    lastScrollTop.current = scrollTop;
+                };
+
+                useEffect(() => {
+                    window.addEventListener('wheel', handleWheel);
+                    window.addEventListener('scroll', handleScroll);
+
+                    return () => {
+                        window.removeEventListener('wheel', handleWheel);
+                        window.removeEventListener('scroll', handleScroll);
+                    };
+                }, []);
+
+                return (
+                    <nav
+                        ref={navbarRef}
+                        aria-label={translate({
+                            id: 'theme.NavBar.navAriaLabel',
+                            message: 'Main',
+                            description: 'The ARIA label for the main navigation',
+                        })}
+                        className={clsx(
+                            'navbar',
+                            'navbar--fixed-top',
+                            isHomePage && 'bg-transparent',
+                            hideOnScroll && [styles.navbarHideable, !isNavbarVisible && styles.navbarHidden],
+                            {
+                                'navbar--dark': style === 'dark',
+                                'navbar--primary': style === 'primary',
+                                'navbar-sidebar--show': mobileSidebar.shown,
+                            },
+                        )}
+                    >
+                        {children}
+                        <NavbarBackdrop onClick={mobileSidebar.toggle} />
+                        <NavbarMobileSidebar />
+                    </nav>
+                );
+            }}
+        </BrowserOnly>
     );
 }
