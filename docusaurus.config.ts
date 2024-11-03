ab202706b97b26c10ff598ec26729d897951e13f
Lian <153711411@qq.com>
2024-11-04 02:31:40 +0800

    feat: update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -132,9 +132,9 @@ const config: Config = {
         {
           title: '声明',
           items: [
-            { label: '用户协议', to: '/agreement' },
             { label: '隐私政策', to: '/privacy' },
             { label: '版权声明', to: '/copyright' },
+            { label: '用户协议', to: '/agreement' },
             { label: 'Cookies', to: '/cookies' },
           ],
         },
